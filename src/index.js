06872ec715bbd0e444bf7270b49a59d236c1e6a5
unknown <lerk1512@gmail.com>
2021-03-08 17:00:34 +0300

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+
+  let result = Math.min.apply(Math, array);
+  if (result == Infinity) {
+    return 0;
+  } else return result;
+
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  let result = Math.max.apply(Math, array);
+  if (result === -Infinity) {
+    return 0;
+  } else return result;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array === undefined) {
+    return 0;
+  } else
+  if (array.length === 0) {
+    return 0;
+  } else
+  let sum = 0;
+  let result = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  result = sum / array.length;
+  return +result;
 }
